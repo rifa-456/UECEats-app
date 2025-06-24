f5904b2a9fdc1195cc36745846f863b7109d297d
Andre Lucio Rocha <andrerocha0911@gmail.com>
2025-06-24 12:58:40 -0300

    feat: componente input text, com react hook form e validation

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,21 +1,19 @@
+import CameraIcon from "./camera";
 import CheckIcon from "./check";
 import CloseIcon from "./close";
-import CameraIcon from "./camera";
 import EarthIcon from "./earth";
 import EllipsisIcon from "./ellipsis";
 import EyeIcon from "./eye";
-import GameControllerIcon from "./game-controller";
 import GalleryVerticalIcon from "./galery-vertical";
 import GridIcon from "./grid";
 import HorizontalIcon from "./horizontal";
-import ListIcon from "./list";
 import MinusLineIcon from "./line";
+import ListIcon from "./list";
 import SearchIcon from "./search";
 import ShieldIcon from "./shield";
 import SquarePenIcon from "./square-pen";
-import UserIcon from "./user";
 import TrashIcon from "./trash";
+import UserIcon from "./user";
 import VerticalIcon from "./vertical";
 
-export { CameraIcon, CheckIcon, CloseIcon, EarthIcon, EllipsisIcon, EyeIcon, HorizontalIcon, GameControllerIcon, 
-    GalleryVerticalIcon, GridIcon, ListIcon, MinusLineIcon, SearchIcon, ShieldIcon, SquarePenIcon, TrashIcon, UserIcon, VerticalIcon};
+export { CameraIcon, CheckIcon, CloseIcon, EarthIcon, EllipsisIcon, EyeIcon, GalleryVerticalIcon, GridIcon, HorizontalIcon, ListIcon, MinusLineIcon, SearchIcon, ShieldIcon, SquarePenIcon, TrashIcon, UserIcon, VerticalIcon };
